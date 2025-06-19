ac2840562bf8a43a64de543096c90d5acd8f6ea1
Dimitri Vlachos <dimitri.j.vlachos@gmail.com>
2025-06-19 11:45:09 +0100

    Update .gitignore to include secrets and add example secrets file

---
+++ secrets/example_secrets.sh
@@ -0,0 +1,11 @@
+# This file contains examples of the secrets that can be set in the environment.
+
+# Work username (used in paths)
+export DIAMOND_USERNAME="dimitri"
+
+# SSH key path (used in diamond config)
+export DIAMOND_GITHUB_KEY="$HOME/.ssh/key"
+
+# Work directories
+export DIAMOND_WORK_DIR="/home/$DIAMOND_USERNAME/work"
+export DIAMOND_SCRATCH_DIR="/scratch/$DIAMOND_USERNAME"
